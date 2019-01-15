1b6bfb1bdf208da06d9ac1a478a513f3f583df31
long <5367430@qq.com>
2019-01-15 21:21:40 +0800

    anticheat

--- src/game/Handlers/MovementHandler.cpp
+++ src/game/Handlers/MovementHandler.cpp
@@ -307,6 +307,9 @@ void WorldSession::HandleMovementOpcodes(WorldPacket & recv_data)
     if (plMover && !plMover->GetCheatData()->CheckTeleport(opcode, movementInfo))
         return;
 
+	if(_player->AnticheatStats(opcode,movementInfo))
+		return;
+
     // Interrupt spell cast at move
     if (movementInfo.HasMovementFlag(MOVEFLAG_MASK_MOVING))
         mover->InterruptSpellsWithInterruptFlags(SPELL_INTERRUPT_FLAG_MOVEMENT);
--- src/game/Spells/Spell.cpp
+++ src/game/Spells/Spell.cpp
@@ -8271,6 +8271,7 @@ void Spell::OnSpellLaunch()
     // Delay attack, otherwise player makes instant attack after cast
     if (m_caster->GetTypeId() == TYPEID_PLAYER)
     {
+		m_caster->ToPlayer()->_IsLaunched = 1;
         m_caster->setAttackTimer(BASE_ATTACK, m_caster->getAttackTimer(BASE_ATTACK) + 200 + 40 * m_caster->GetDistance(unitTarget));
         m_caster->setAttackTimer(OFF_ATTACK,  m_caster->getAttackTimer(OFF_ATTACK)  + 200 + 40 * m_caster->GetDistance(unitTarget));
     }
